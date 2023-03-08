a51bf07253b6f1b514baeacee7426fd815fcda91
Terrero <sergio-terrero@hotmail.com>
2023-03-07 23:05:39 -0500

    first commit

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,22 @@
+import React, {Fragment} from 'react';
+import mealsImg from '../../assets/photo-1606787366850-de6330128bfc.avif'
+import classes from './Header.module.css'
+
+const Header = (props) => {
+
+    return (<Fragment>
+
+        <header className={classes.header}>
+            <h1>Order Meals</h1>
+            <button>Cart</button>
+        </header>
+            <div className={classes['main-image']}>
+                <img src={mealsImg} alt="Delicious food"/>
+            </div>
+
+
+    </Fragment>)
+
+}
+
+export default Header
