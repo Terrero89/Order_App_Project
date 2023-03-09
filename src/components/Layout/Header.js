1d616e496639654ebcfdd9e748e57e0df2978803
Terrero <sergio-terrero@hotmail.com>
2023-03-08 20:53:32 -0500

    items display section

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,6 +1,7 @@
 import React, {Fragment} from 'react';
 import mealsImg from '../../assets/photo-1606787366850-de6330128bfc.avif'
 import classes from './Header.module.css'
+import HeaderCartButton from "./HeaderCartButton";
 
 const Header = (props) => {
 
@@ -8,7 +9,7 @@ const Header = (props) => {
 
         <header className={classes.header}>
             <h1>Order Meals</h1>
-            <button>Cart</button>
+            <HeaderCartButton/>
         </header>
             <div className={classes['main-image']}>
                 <img src={mealsImg} alt="Delicious food"/>
---
+++ src/components/Meals/MealsList.js
@@ -0,0 +1,12 @@
+import React, {Fragment } from 'react';
+
+const MealsList = (props)=>{
+
+    return <Fragment>
+
+        <li>{props.list}</li>
+    </Fragment>
+
+}
+
+export default MealsList
